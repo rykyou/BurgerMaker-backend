c9fa5f519f511e10d1a83bdc7216c7566354de85
vovka26 <317110@gmail.com>
2019-01-22 17:56:50 -0500

    implemented full CRUD

--- app/controllers/burgers_controller.rb
+++ app/controllers/burgers_controller.rb
@@ -3,4 +3,23 @@ class BurgersController < ApplicationController
     render json: Burger.all
   end
 
+  def create
+    render json: Burger.create(burger_params)
+  end
+
+  def update
+    Burger.find(params[:id]).update(burger_params)
+    render json: Burger.find(params[:id])
+  end
+
+  def destroy
+    render json: Burger.find(params[:id]).destroy
+  end
+
+  private
+
+  def burger_params
+    params.require(:burger).permit(:name, :owner_name, :ingredients)
+  end
+
 end
